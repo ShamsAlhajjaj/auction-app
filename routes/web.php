697483829d0e389d8bb40440c655746db6b32fbd
ShamsAlhajjaj <62596487+ShamsAlhajjaj@users.noreply.github.com>
2024-10-27 00:39:15 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,40 +1,4 @@
 <?php
-
-// use App\Http\Controllers\BidController;
-// use App\Http\Controllers\ProductController;
-// use App\Http\Controllers\ProfileController;
-// use Illuminate\Support\Facades\Route;
-
-// Route::get('/products', [ProductController::class, 'index'])->name('index');
-// Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
-// Route::get('/products/create', function () { return view('products.store');})->name('products.create');
-// Route::post('/bids/store', [BidController::class, 'store'])->name('bids.store');
-// Route::get('/products/{productId}', [ProductController::class, 'show'])->name('products.details');
-// Route::get('/myProducts', [ProductController::class, 'myProducts'])->name('myProducts');
-// Route::delete('/bids/{id}', [BidController::class, 'destroy'])->name('bids.destroy');
-
-
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-// require __DIR__.'/auth.php';
-
-
-
 use App\Http\Controllers\BidController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
